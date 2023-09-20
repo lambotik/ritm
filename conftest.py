ff6987e830555e77f15c8690271ce5f612ec80c7
QA/AQA Python Dzmitry Charukha <91555504+lambotik@users.noreply.github.com>
2023-09-20 18:00:53 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,13 +1,20 @@
+from datetime import datetime
+import allure
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture()
 def driver():
+    options = Options()
+    options.add_argument('--headless')
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-    driver.maximize_window()
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.implicitly_wait(10)
     yield driver
+    attach = driver.get_screenshot_as_png()
+    allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG)
     driver.quit()
